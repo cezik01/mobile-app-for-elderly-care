fcf9a026b869a947ec54ec3799548b7dec633ee1
xebelge <edabelge00@gmail.com>
2023-12-12 22:16:48 +0300

    update profile edit screen as role-based

--- src/screens/profiles/ProfileEditScreen.tsx
+++ src/screens/profiles/ProfileEditScreen.tsx
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
-import { getAuth } from 'firebase/auth';
-import { getDatabase, ref, onValue ,update} from 'firebase/database';
+import { getAuth, User } from 'firebase/auth';
+import { getDatabase, ref, onValue, update, DatabaseReference } from 'firebase/database';
 import { useNavigation } from '@react-navigation/native';
+
+interface UserData {
+  name: string;
+  surname?: string;
+  city: string;
+  age?: string;
+  height?: string;
+  weight?: string;
+  bloodType?: string;
+  role?: string;
+}
 
 const ProfileEditScreen = () => {
   const navigation = useNavigation();
@@ -13,60 +24,69 @@ const ProfileEditScreen = () => {
   const [height, setHeight] = useState('');
   const [weight, setWeight] = useState('');
   const [bloodType, setBloodType] = useState('');
-  
+  const [role, setRole] = useState('');
 
   const auth = getAuth();
-  const user = auth.currentUser;
+  const user: User | null = auth.currentUser;
   const db = getDatabase();
 
   useEffect(() => {
     if (user) {
-      const userRef = ref(db, 'users/' + user.uid);
+      const userRef: DatabaseReference = ref(db, 'users/' + user.uid);
       onValue(userRef, (snapshot) => {
-        const data = snapshot.val();
+        const data: UserData = snapshot.val() as UserData;
         setName(data.name);
-        setSurname(data.surname);
+        setSurname(data.surname || '');
         setCity(data.city);
-        setAge(data.age);
-        setHeight(data.height);
-        setWeight(data.weight);
-        setBloodType(data.bloodType);
-        
+        setAge(data.age || '');
+        setHeight(data.height || '');
+        setWeight(data.weight || '');
+        setBloodType(data.bloodType || '');
+        setRole(data.role || '');
       });
     }
-  }, []);
+  }, [user, db]);
 
   const handleSave = async () => {
-    if (user) {
-      const updates: { [key: string]: any } = {};
-      updates['/users/' + user.uid + '/name'] = name;
-      updates['/users/' + user.uid + '/surname'] = surname;
-      updates['/users/' + user.uid + '/city'] = city;
-      updates['/users/' + user.uid + '/age'] = age;
-      updates['/users/' + user.uid + '/height'] = height;
-      updates['/users/' + user.uid + '/weight'] = weight;
-      updates['/users/' + user.uid + '/bloodType'] = bloodType;
-      
-      
-  
-      await update(ref(db), updates);
-      Alert.alert('Profile Updated', 'Your profile has been updated successfully.', [
-        { text: "OK", onPress: () => navigation.goBack() } 
+    try {
+      if (user) {
+        const updates: Record<string, any> = {};
+        updates['/users/' + user.uid + '/name'] = name;
+        updates['/users/' + user.uid + '/city'] = city;
+
+        // Only update other fields if they are not empty
+        if (surname) updates['/users/' + user.uid + '/surname'] = surname;
+        if (age) updates['/users/' + user.uid + '/age'] = age;
+        if (height) updates['/users/' + user.uid + '/height'] = height;
+        if (weight) updates['/users/' + user.uid + '/weight'] = weight;
+        if (bloodType) updates['/users/' + user.uid + '/bloodType'] = bloodType;
+
+        await update(ref(db), updates);
+        Alert.alert('Profile Updated', 'Your profile has been updated successfully.', [
+          { text: "OK", onPress: () => navigation.goBack() }
+        ]);
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Update Failed', 'There was an error updating your profile.', [
+        { text: "OK" }
       ]);
-      
-      
     }
   };
 
   return (
     <View>
       <TextInput placeholder="Name" value={name} onChangeText={setName} />
-      <TextInput placeholder="Surname" value={surname} onChangeText={setSurname} />
       <TextInput placeholder="City" value={city} onChangeText={setCity} />
-      <TextInput placeholder="Age" value={age} onChangeText={setAge} />
-      <TextInput placeholder="Height" value={height} onChangeText={setHeight} />
-      <TextInput placeholder="Weight" value={weight} onChangeText={setWeight} />
-      <TextInput placeholder="Blood Type" value={bloodType} onChangeText={setBloodType} />
+      {role === 'patients' && (
+        <>
+          <TextInput placeholder="Surname" value={surname} onChangeText={setSurname} />
+          <TextInput placeholder="Age" value={age} onChangeText={setAge} />
+          <TextInput placeholder="Height" value={height} onChangeText={setHeight} />
+          <TextInput placeholder="Weight" value={weight} onChangeText={setWeight} />
+          <TextInput placeholder="Blood Type" value={bloodType} onChangeText={setBloodType} />
+        </>
+      )}
       <Button title="Save" onPress={handleSave} />
     </View>
   );
