8be62d836fff8ab0ceada67ddbe970a3ed1d005a
xebelge <edabelge00@gmail.com>
2024-01-06 19:52:29 +0300

    format

--- src/context/FontSizeContext.tsx
+++ src/context/FontSizeContext.tsx
@@ -5,22 +5,21 @@ import { FontSizeProviderProps } from 'types/FontSizeProviderProps';
 
 const FontSizeContext = createContext<{ fontSize: string; setFontSize: (fontSize: string) => void }>({
   fontSize: 'large',
-  setFontSize: () => {}, 
+  setFontSize: () => { },
 });
 
 export const FontSizeProvider = ({ children }: FontSizeProviderProps) => {
   const [fontSize, setFontSize] = useState('large');
-  
 
   useEffect(() => {
     const auth = getAuth();
     const db = getDatabase();
-    let unsubscribe: (() => void) | null = null; 
-    
+    let unsubscribe: (() => void) | null = null;
+
     if (auth.currentUser) {
       const userId = auth.currentUser.uid;
       const fontSizeRef = ref(db, `users/${userId}/preferences/fontSize`);
-      
+
       unsubscribe = onValue(fontSizeRef, (snapshot) => {
         if (snapshot.exists()) {
           const newFontSize = snapshot.val();
@@ -28,15 +27,13 @@ export const FontSizeProvider = ({ children }: FontSizeProviderProps) => {
         }
       });
     }
-    
+
     return () => {
       if (unsubscribe) unsubscribe();
     };
   }, []);
-  
-  
-  console.log('FontSizeProvider values:', { fontSize, setFontSize });
 
+  console.log('FontSizeProvider values:', { fontSize, setFontSize });
 
   return (
     <FontSizeContext.Provider value={{ fontSize, setFontSize }}>
