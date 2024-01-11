04c7f1dd2c1359fae21b0cf3fd770adab43fbba4
xebelge <edabelge00@gmail.com>
2024-01-11 09:26:17 +0300

    update sidebar feedback

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -34,7 +34,11 @@ export const Sidebar = ({ role, style, setSidebarVisible, navigation, handleLogo
                 <Text style={styles.helpInfoText}>{i18n.t('HelpInfo')}</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={handleFeedbackPress}>
-                <Text style={styles.feedbackText}>{i18n.t('Feedback')}</Text>
+                <View style={styles.feedbackContainer}>
+                    <MaterialIcons name='feedback' style={styles.feedbackIcon} size={25} />
+                    <Text style={styles.feedbackText}>{i18n.t('Feedback')}</Text>
+                </View>
+                <Text style={styles.feedbackInfoText}>{i18n.t('FeedbackInfo')}</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => {
                 handleLogout().then(() => {
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -63,7 +63,19 @@ export const styles = StyleSheet.create({
     feedbackText: {
         color: '#fff',
         fontSize: 20,
+    },
+    feedbackContainer: {
+        flexDirection: 'row',
         marginTop: 100,
+    },
+    feedbackIcon: {
+        color: '#fff',
+        marginRight: 5,
+    },
+    feedbackInfoText: {
+        marginTop: 20,
         marginLeft: 5,
+        color: '#fff',
+        fontSize: 20,
     },
 });
