61bc3bf30dbc61a2504a2030d605694b1f241bf5
xebelge <edabelge00@gmail.com>
2024-01-17 13:35:21 +0300

    refactor

--- src/screens/auth/styles.ts
+++ src/screens/auth/styles.ts
@@ -1,5 +1,7 @@
 import { theme } from 'common/theme/theme';
+import { heightPercentageToDP, widthPercentageToDP } from 'helpers/dimension';
 import { StyleSheet } from 'react-native';
+
 
 const { spacing, colorScheme } = theme.components;
 
@@ -7,6 +9,18 @@ export const styles = StyleSheet.create({
   linkText: {
     color: colorScheme.light.primary.purpleBunny[100],
     marginTop: spacing[5],
+  },
+  pickerContainer: {
+    width: widthPercentageToDP('100%'),
+    height: heightPercentageToDP('8%'),
+    paddingHorizontal: spacing[3],
+    marginVertical: spacing[2],
+    backgroundColor: colorScheme.light.primary.canvasBunny,
+    color: colorScheme.light.primary.purpleBunny[100],
+    fontSize: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
   },
 });
 
